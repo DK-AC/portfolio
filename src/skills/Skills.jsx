d0828f880f2de34d494e2025e145a5d4ef84d81e
DK-AC <denkacaj@gmail.com>
2022-11-06 21:39:03 +0300

    skill created

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import style from './Skills.module.css'
+import style from './Skills.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
 import {Skill} from './skill'
 import {Title} from '../common/components/title'
+import reactIcon from './../assets/icons/react-svgrepo-com.svg'
+import typescriptIcon from './../assets/icons/typescript-svgrepo-com.svg'
+import materialIcon from './../assets/icons/material-ui-svgrepo-com.svg'
+import githubIcon from './../assets/icons/github-svgrepo-com.svg'
+import scssIcon from './../assets/icons/scss2-svgrepo-com.svg'
+import bootstrapIcon from './../assets/icons/bootstrap-svgrepo-com.svg'
+import reduxIcon from './../assets/icons/redux-svgrepo-com.svg'
+import jestIcon from './../assets/icons/jest-svgrepo-com.svg'
+import axiosIcon from './../assets/icons/axios-seeklogo.com.svg'
 
 export const Skills = () => {
 
@@ -10,15 +19,49 @@ export const Skills = () => {
 
     return (
         <div className={style.skillsBlock}>
-            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+            <div className={`${styleContainer.container} ${style.container}`}>
                 <Title name={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill title={'HTML'}
+                    <Skill title={'react'}
+                           description={loremDescription}
+                           icon={reactIcon}
+                    />
+                    <Skill title={'redux thunk / toolkit'}
+                           description={loremDescription}
+                           icon={reduxIcon}
+                    />
+                    <Skill title={'javascript / ts'}
+                           description={loremDescription}
+                           icon={typescriptIcon}
+                    />
+                    <Skill title={'axios / rest api'}
+                           description={loremDescription}
+                           icon={axiosIcon}
+                    />
+                    <Skill title={'material / ant'}
+                           description={loremDescription}
+                           icon={materialIcon}
+                    />
+                    <Skill title={'react hooks'}
                            description={loremDescription}/>
-                    <Skill title={'CSS'}
-                           description={loremDescription}/>
-                    <Skill title={'React'}
-                           description={loremDescription}/>
+                    <Skill title={'html & css / scss'}
+                           description={loremDescription}
+                           icon={scssIcon}
+                    />
+                    <Skill title={'bem / bootstrap'}
+                           description={loremDescription}
+                           icon={bootstrapIcon}
+                    />
+                    <Skill title={'tdd / jest / storybook'}
+                           description={loremDescription}
+                           icon={jestIcon}
+                    />
+
+                    <Skill title={'git / github'}
+                           description={loremDescription}
+                           icon={githubIcon}
+                    />
+
                 </div>
             </div>
 
--- src/skills/skill/Skill.jsx
+++ src/skills/skill/Skill.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import style from './Skill.module.css'
+import style from './Skill.module.scss'
 
-export const Skill = ({title,description}) => {
+export const Skill = ({title, description, icon}) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
-            <h3 className={style.title}>{title}</h3>
-            <span className={style.description}>{description}</span>
+            <img className={style.icon} src={icon} alt="skill logo" />
+            <div className={style.info}>
+                <h3 className={style.title}>{title}</h3>
+                <span className={style.description}>{description}</span>
+            </div>
         </div>
     )
 }
