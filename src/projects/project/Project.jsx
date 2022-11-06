3ddcd052564d1aa9f115169453bd60eb60b5f080
DK-AC <denkacaj@gmail.com>
2022-11-06 16:19:13 +0300

    created mixins and variables refactoring scss files

--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -4,10 +4,10 @@ import styleProject from './Project.module.scss'
 export const Project = ({title, description, style}) => {
     return (
         <div className={styleProject.project}>
-            <div className={styleProject.projectImage} style={style}>
+            <div className={styleProject.image} style={style}>
                 <a href="/" className={styleProject.viewBtn}>Watch</a>
             </div>
-            <div className={styleProject.projectInfo}>
+            <div className={styleProject.info}>
                 <h3 className={styleProject.title}>{title}</h3>
                 <span className={styleProject.description}>{description}</span>
             </div>
