a7dc475a16728a0acaae87ce2bd830a275b37278
DK-AC <denkacaj@gmail.com>
2022-11-07 19:33:48 +0300

    footer adaptive

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import styleContainer from './../common/styles/Container.module.css'
-import style from './Footer.module.css'
+import style from './Footer.module.scss'
 
 export const Footer = () => {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.text}>Denis Katsai</h2>
-                <div className={style.socialIcons}>
-                    <div className={style.socialIcon}>1</div>
-                    <div className={style.socialIcon}>2</div>
-                    <div className={style.socialIcon}>3</div>
-                    <div className={style.socialIcon}>4</div>
-                    <div className={style.socialIcon}>5</div>
-                    <div className={style.socialIcon}>6</div>
-                </div>
-                <span className={style.text}>&copy; 2022 Все права защищены.</span>
+                <h2 className={style.logo}>Denis</h2>
+                <ul className={style.socialLinks}>
+                    <li><a href={'https://www.linkedin.com/in/denis-katsai-254a0720a/'}
+                           className={style.socialIcon}>linkedin</a></li>
+                    <li><a href={'https://t.me/Dk_Auto_Chess'}
+                           className={style.socialIcon}>telegram</a></li>
+                    <li><a href={'https://github.com/DK-AC'}
+                           className={style.socialIcon}>github</a></li>
+
+                </ul>
+                <span className={style.copyright}>&copy; 2022 Все права защищены.</span>
             </div>
         </div>
     )
