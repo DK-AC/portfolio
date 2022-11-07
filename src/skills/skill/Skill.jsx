99501e727cb7c2acedbc325ae901886eafef7c4c
DK-AC <denkacaj@gmail.com>
2022-11-07 11:50:50 +0300

    square rotate skill

--- src/skills/skill/Skill.jsx
+++ src/skills/skill/Skill.jsx
@@ -4,7 +4,9 @@ import style from './Skill.module.scss'
 export const Skill = ({title, description, icon}) => {
     return (
         <div className={style.skill}>
-            <img className={style.icon} src={icon} alt="skill logo" />
+            <div className={style.square}>
+                <img className={style.icon} src={icon} alt="skill logo"/>
+            </div>
             <div className={style.info}>
                 <h3 className={style.title}>{title}</h3>
                 <span className={style.description}>{description}</span>
