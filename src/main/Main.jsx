0a41a16b60446cd6eed35a2bf9f61da742013494
DK-AC <denkacaj@gmail.com>
2022-11-07 16:04:17 +0300

    main page is ready

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import style from './Main.module.css'
+import style from './Main.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
+import avatar from './../assets/image/avatar.jpg'
 
 export const Main = () => {
     return (
-        <div className={style.main}>
-            <div className={` ${style.container} ${styleContainer.container}`}>
-                <div className={style.description}>
-                    <span>Hi there</span>
-                    <h1>I am Denis Katsai</h1>
-                    <p>Frontend Developer</p>
-                </div>
-                <div className={style.photo}>
-                    photo
+        <div
+            className={`${style.heroBannerContent} ${style.vAlignCenter} ${style.dFlex}`}>
+            <div className={styleContainer.container}>
+                <div className={style.dTable}>
+                    <div
+                        className={`${style.heroBannerLeft} ${style.dTableCell} ${style.vAlignMiddle}`}>
+                        <div className={style.intro}>
+                            <span>Hi there</span>
+                            <h1>I am <span>Denis Katsai</span>
+                            </h1>
+                            <p>A Frontend Developer</p>
+                        </div>
+                    </div>
+                    <div
+                        className={`${style.heroBannerRight} ${style.dTableCell} ${style.vAlignMiddle}`}>
+                        <div className={style.heroRight}>
+                            <img src={avatar} alt="avatar"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
