3ba797d6c0bff6bb6cf92dac837be3ceb39e344e
DK-AC <denkacaj@gmail.com>
2022-11-09 14:20:08 +0300

    change nav to BurgerNav if width <720px

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import style from './Header.module.scss'
 import {Nav} from './nav'
+import {BurgerNav} from './burgerNav'
 
 export const Header = () => {
+
+    const [isMobile, setIsMobile] = useState(false)
+
+    const handleResize = () => {
+        if (window.innerWidth < 720) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+    })
+
+
     return (
         <div className={style.header}>
-            {/*<BurgerNav/>*/}
-            <Nav/>
+            {isMobile ? <BurgerNav/> : <Nav/>}
         </div>
     )
 }
