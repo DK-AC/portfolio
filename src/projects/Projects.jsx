43948202c542d682a2f9f81d9b263a59f599cb26
DK-AC <denkacaj@gmail.com>
2022-11-10 16:39:50 +0300

    add href to projects

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -6,6 +6,7 @@ import {Title} from '../common/components/title'
 import cards from './../assets/image/Cards.png'
 import socialNetwork from './../assets/image/SocialNetwork.png'
 import movies from './../assets/image/Movies.png'
+import todolist from './../assets/image/Todolist.png'
 
 export const Projects = () => {
 
@@ -18,6 +19,9 @@ export const Projects = () => {
     const MoviesImage = {
         backgroundImage: `url(${movies})`,
     }
+    const TodolistImage = {
+        backgroundImage: `url(${todolist})`,
+    }
 
     return (
         <div id={'projects'} className={style.projectsBlock}>
@@ -25,18 +29,22 @@ export const Projects = () => {
                 <Title name={'projects'}/>
                 <div className={style.projects}>
                     <Project title={'Todolist'}
+                             href={'https://github.com/DK-AC/TodoList'}
                              description={`A web service for managing tasks. Tasks can be placed into projects, sorted by filter, assigned labels, edited.`}
-                             style={cardsImage}
+                             style={TodolistImage}
                     />
                     <Project title={'Social Network'}
                              description={`An online platform that is used for communication, dating, creating social relationships between people.`}
                              style={socialNetworkImage}
+                             href={'https://github.com/DK-AC/social-network-ts'}
                     />
                     <Project title={'Movies'}
                              description={`Movie and TV Series Search Site`}
                              style={MoviesImage}
+                             href={'https://github.com/DK-AC/movies'}
                     />
                     <Project title={'Cards'}
+                             href={'https://github.com/DK-AC/Cards'}
                              description={`Cards is a learning application for memorizing information using flashcards.`}
                              style={cardsImage}
                     />
--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styleProject from './Project.module.scss'
 
-export const Project = ({title, description, style}) => {
+export const Project = ({title, description, style, href}) => {
     return (
         <div className={styleProject.project}>
             <div className={styleProject.image} style={style}>
-                <a href="/" className={styleProject.viewBtn}>Watch</a>
+                <a href={href} className={styleProject.viewBtn}>Watch</a>
             </div>
             <div className={styleProject.info}>
                 <h3 className={styleProject.title}>{title}</h3>
